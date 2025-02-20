1868d88902f82f49e3cd582dcd71e56202e27e28
jhoyos <hoyos.juan.2961@eam.edu.co>
2025-02-20 17:34:09 -0500

    agregado funcion multiplicacion

--- src/main/java/com/eam/app/Calculadora.java
+++ src/main/java/com/eam/app/Calculadora.java
@@ -8,4 +8,8 @@ public class Calculadora {
 	public static int resta(int a, int b) {
         return a - b;
     }
+	
+	public static int multiplicacion(int a, int b) {
+        return a * b;
+    }
 }
