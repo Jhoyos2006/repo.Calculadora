4f492d346c2c33ec18325e68caebd03313f8a679
jhoyos <hoyos.juan.2961@eam.edu.co>
2025-02-20 17:35:16 -0500

    agregado funcion division

--- src/main/java/com/eam/app/Calculadora.java
+++ src/main/java/com/eam/app/Calculadora.java
@@ -12,4 +12,11 @@ public class Calculadora {
 	public static int multiplicacion(int a, int b) {
         return a * b;
     }
+	
+	public static double dividir(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+        return (double) a / b;
+    }
 }
